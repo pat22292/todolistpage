95cff30480f78c426633ad012a36f8ce4e740d4e
pat22292 <madlangawapat22@gmail.com>
2020-09-29 18:27:56 +0800

    test

---
+++ precache-manifest.d9d34e90cb9f6c7942f74a1ece1b3cc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766572f0a12950395d0b5cfca8d7c5f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38ce855a27484237b2a4",
+    "url": "/static/js/2.aedb2519.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aedb2519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a92b9cd43c20f20ff29",
+    "url": "/static/js/main.1c85759b.chunk.js"
+  },
+  {
+    "revision": "c25be902f93ad91c2984",
+    "url": "/static/js/runtime-main.1b9deb29.js"
+  }
+]);
